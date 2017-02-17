ef33f64e442c5ebd8a37a400ba38487d59418f97
tvmendoza <tonamendoza@ufl.edu>
2017-02-17 14:18:33 -0500

    Updated the page to have all the publications that have a grant, created a menu to move around the page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  * # MainCtrl
  * Controller of the obc-grants
  */
-angular.module('dujourApp')
+angular.module('dujourApp') 
     .controller('MainCtrl', function ($scope, $location, $http) {
 
         var APIURL = 'http://localhost:3000';
@@ -22,6 +22,7 @@ angular.module('dujourApp')
             var publications = {};
             var pubsByYear = {}
             //The first loop gets rid of duplicates
+            var total = 0;
             for (var i in data) {
                 if (data[i].pubName != undefined){
                     var key = data[i].pubName
@@ -36,12 +37,12 @@ angular.module('dujourApp')
                             idLabel: data[i].grantidlabel
                         };
                         publications[key] = current
+                        total = total + 1;
                     }
                 }
-                
             }
             //The second loop puts the values by year.
-            console.log(publications)
+            
             for (var key in publications) { 
                 var year = publications[key].published.split("-")[0]
                 var pub = [publications[key]]
@@ -50,18 +51,24 @@ angular.module('dujourApp')
                 }
                 else {
                     pubsByYear[year].push(publications[key]);
-                }
+                } 
             }
+            
             $http.get('scripts/configs/publications.json')
             .success(function (config) {
                 //set the publications by the dates we get from the json config file.
                 var dates = config.dates;
                 $scope.title =config.title;
-                console.log(dates)
+
                 for (var i in dates) {
-                    var header = { "title": dates[i].date};
-                    header.items = []
                     var datesRange = dates[i].date.split("-");
+                    var title =dates[i].date;
+                    if (datesRange.length > 1) {
+                        var title = datesRange[1] + "-" + datesRange[0];
+                    }
+                    var header = {};
+                    header.items = []
+                    header.title = title
                     if (datesRange.length > 1) {
                         //This is a range of dates
                         var range = datesRange[0]- datesRange[1];
@@ -70,27 +77,45 @@ angular.module('dujourApp')
                             if(currentDate in pubsByYear){
                                 pubsByYear[currentDate].sort(function(a,b) {return (a.published < b.published) ? 1 : ((b.published < a.published) ? -1 : 0);} );
                                 for(pub in pubsByYear[currentDate]){
-                                     header.items = header.items.concat(pubsByYear[currentDate][pub])
+                                    total = total-1
+                                    pubsByYear[currentDate][pub].index = total;
+                                    header.items = header.items.concat(pubsByYear[currentDate][pub])
                             }
                         }
                       }
                     }
                     else {
-                        //This is just one year
+                         //This is just one year
                         var currentDate = dates[i].date
                         if(currentDate in pubsByYear){
                             pubsByYear[currentDate].sort(function(a,b) {return (a.published < b.published) ? 1 : ((b.published < a.published) ? -1 : 0);} );
                             for( pub in pubsByYear[currentDate] ){
-                                console.log(pubsByYear[currentDate][pub])
-                                header.items = header.items.concat(pubsByYear[currentDate][pub])
+                                total = total-1
+                                pubsByYear[currentDate][pub].index = total;
+                                header.items = header.items.concat(pubsByYear[currentDate][pub])   
                             }
                         }
                     }
-                    $scope.headers.push(header)
+                    if(header.items.length >0){
+                        $scope.headers.push(header)
+                    }
                 }
            });
            
         });
-    });
+    })
+.controller('headerCtrl', ['$anchorScroll', '$location', '$scope',
+  function($anchorScroll, $location, $scope) {
+    $scope.gotoAnchor = function(x) {
+      var newHash = 'anchor' + x;
+      if ($location.hash() !== newHash) {
+        $anchorScroll(newHash);
+      } else {
+        $anchorScroll();
+      }
+    };
+  }
+]);
+
 
 
